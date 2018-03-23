b636216a7c81b6253c025344ef45d40a7864d5c0
Divya Mamgai <divyamamgai21@gmail.com>
2018-03-23 12:37:04 +0530

    Minor API Changes

--- api/views.py
+++ api/views.py
@@ -13,16 +13,19 @@ def auth_sign_up(request):
     """
     API endpoint that signs up a user if not already existing.
     """
-    if request.method == 'POST':
-        serializer = serializers.UserCreateSerializer(data=request.data)
-        if serializer.is_valid():
+    serializer = serializers.UserCreateSerializer(data=request.data)
+    if serializer.is_valid():
+        if request.method == 'POST':
             serializer.save()
             data = serializer.data
             data.pop('password')
             return Response(data, status=status.HTTP_201_CREATED)
+        elif request.method == 'HEAD':
+            return Response(content_type='text/json',
+                            headers=('content-length', len(serializer.data)),
+                            status=status.HTTP_200_OK)
+    else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'HEAD':
-        pass
 
 
 @api_view(['POST', 'HEAD'])
@@ -31,10 +34,19 @@ def auth_login(request):
     API endpoint that logs in the user if the email and password combination
     is correct.
     """
+    email = request.data.get('email', None)
+    password = request.data.get('password', None)
+    user_type = request.data.get('user_type', None)
+    if email is None:
+        return Response({'detail': 'email field is required.'},
+                        status=status.HTTP_400_BAD_REQUEST)
+    if password is None:
+        return Response({'detail': 'password field is required.'},
+                        status=status.HTTP_400_BAD_REQUEST)
+    if user_type is None:
+        return Response({'detail': 'user_type field is required.'},
+                        status=status.HTTP_400_BAD_REQUEST)
     if request.method == 'POST':
-        email = request.data.get('email')
-        password = request.data.get('password')
-        user_type = request.data.get('user_type')
         user = authenticate(username=email, password=password)
         if user is not None:
             if user.is_active and user.user_type == user_type:
@@ -49,7 +61,7 @@ def auth_login(request):
                 'detail': 'Email and password combination is incorrect.'},
                 status=status.HTTP_401_UNAUTHORIZED)
     elif request.method == 'HEAD':
-        pass
+        return Response(status=status.HTTP_200_OK)
 
 
 @api_view(['POST', 'HEAD'])
@@ -63,9 +75,7 @@ def auth_logout(request):
         logout(request)
         return Response(status=status.HTTP_200_OK)
     elif request.method == 'HEAD':
-        pass
-    elif request.method == 'OPTIONS':
-        pass
+        return Response(status=status.HTTP_200_OK)
 
 
 @api_view(['GET', 'HEAD'])
@@ -74,17 +84,19 @@ def get_mentors(request):
     """
     API endpoint that allows mentors to be viewed.
     """
-    if request.method == 'GET':
-        if request.user.user_type == 'mentee':
-            mentors = User.objects.filter(user_type='mentor')
-            serializer = serializers.UserSerializer(mentors, many=True)
-            return Response(serializer.data)
-        else:
-            return Response(
-                data={"detail": "Only mentees can access this resource."},
-                status=status.HTTP_403_FORBIDDEN)
-    elif request.method == 'HEAD':
-        pass
+    if request.user.user_type == 'mentee':
+        mentors = User.objects.filter(user_type='mentor')
+        serializer = serializers.UserSerializer(mentors, many=True)
+        if request.method == 'GET':
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        elif request.method == 'HEAD':
+            return Response(content_type='text/json',
+                            headers=('content-length', len(serializer.data)),
+                            status=status.HTTP_200_OK)
+    else:
+        return Response(
+            data={'detail': 'Only mentees can access this resource.'},
+            status=status.HTTP_403_FORBIDDEN)
 
 
 @api_view(['GET', 'HEAD'])
@@ -93,14 +105,16 @@ def get_mentees(request):
     """
     API endpoint that allows mentees to be viewed.
     """
-    if request.method == 'GET':
-        if request.user.user_type == 'mentor':
-            mentees = User.objects.filter(user_type='mentee')
-            serializer = serializers.UserSerializer(mentees, many=True)
-            return Response(serializer.data)
-        else:
-            return Response(
-                data={"detail": "Only mentors can access this resource."},
-                status=status.HTTP_403_FORBIDDEN)
-    elif request.method == 'HEAD':
-        pass
+    if request.user.user_type == 'mentor':
+        mentees = User.objects.filter(user_type='mentee')
+        serializer = serializers.UserSerializer(mentees, many=True)
+        if request.method == 'GET':
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        elif request.method == 'HEAD':
+            return Response(content_type='text/json',
+                            headers=('content-length', len(serializer.data)),
+                            status=status.HTTP_200_OK)
+    else:
+        return Response(
+            data={'detail': 'Only mentors can access this resource.'},
+            status=status.HTTP_403_FORBIDDEN)
